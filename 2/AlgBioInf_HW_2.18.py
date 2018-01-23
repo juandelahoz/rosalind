811505d54788f9daa7d896a44610f215ab40db91
juandelahoz <juanfdelahoz@gmail.com>
2018-01-23 15:41:47 -0800

    finished assignment 2, no bonus

--- 2/AlgBioInf_HW_2.18.py
+++ 2/AlgBioInf_HW_2.18.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python
-
 import sys
 
 file = open(sys.argv[1], 'r')
-seqs = file.readlines().strip()
+seqs = file.read().splitlines()
 
-print(seqs)
+trie = []
+initial = seqs[0]
+last = 0
+
+for i in range(len(initial)):
+    trie.append((i+1,i+2,initial[i]))
+    last = i+2
+
+for seq in seqs:
+    path = 1
+    for i in range(len(seq)):
+        in_path = False
+        for node in trie:
+            if node[0] == path and node[2] == seq[i]:
+                path = node[1]
+                in_path = True 
+                break
+        if in_path == False:
+            last += 1
+            trie.append((path,last,seq[i]))
+            path = last
+
+for t in trie:
+    print(str(t[0])+" "+str(t[1])+" "+t[2])
