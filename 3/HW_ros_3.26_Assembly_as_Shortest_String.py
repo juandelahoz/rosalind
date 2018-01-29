a260f78654944678c50171d924973500b525ea3a
juandelahoz <juanfdelahoz@gmail.com>
2018-01-29 11:46:19 -0800

    solved 3.26

--- 3/HW_ros_3.26_Assembly_as_Shortest_String.py
+++ 3/HW_ros_3.26_Assembly_as_Shortest_String.py
@@ -7,89 +7,35 @@ for line in file:
 	if not line.startswith(">"):
 		seqs.append( line.strip() )
 
-print(seqs)
 # initialize variables, start assembling with first read
 assembly = seqs[0]
 seqs.remove(assembly)
 
-print(seqs)
-print(assembly)
+def extend_assembly_shortest(assembly, seqs):
+	next_move = ("",len(assembly),"XX")
+	for seq in seqs:
+		# at least half of the sequence overlaps
+		half = int(len(seq)/2)
+		for i in range(half,len(seq)):
+		# check if as sufix is shorter
+			if assembly[-i:] == seq[:i]:
+				addition = len(seq) - i
+				if next_move[1] > addition:
+					next_move = (seq, addition, "sx")
+		# check if as prefix is shorter
+			if assembly[:i] == seq[-i:]:
+				addition = len(seq) - i
+				if next_move[1] > addition:
+					next_move = (seq, addition, "px")
+				
+	if next_move[2] == "sx":
+		assembly = assembly + next_move[0][-next_move[1]:]
+	elif next_move[2] == "px":
+		assembly = next_move[0][:next_move[1]] + assembly
+	seqs.remove(next_move[0])
+	return assembly,seqs
 
+for i in range(len(seqs)):
+	assembly,seqs = extend_assembly_shortest(assembly, seqs)
 
-next_move = ("",len(assembly),"XX")
-for seq in seqs:
-	# check if as sufix is shorter
-	half = len(seq)/2
-	for i in range(half,len(seq)):
-		if assembly[-i:] == seq[:i]:
-			addition = len(seq) - i
-			if next_move[1] > addition:
-				next_move = (seq, addition, "sx")			
-	# check if as prefix is shorter
-	half = len(seq)/2
-	for i in range(half):
-		if assembly[:i] == seq[-i:]:
-			addition = len(seq) - i
-			if next_move[1] > addition:
-				next_move = (seq, addition, "px")
-			
-if next_move[2] == "sx":
-	assembly = assembly + next_move[0][-next_move[1]:]
-elif next_move[2] == "px":
-	assembly = next_move[0][:next_move[1]] + assembly
-seqs.remove(next_move[0])
-
-print(seqs)
-print(assembly)
-
-next_move = ("",len(assembly),"XX")
-for seq in seqs:
-	# check if as sufix is shorter
-	half = len(seq)/2
-	for i in range(half,len(seq)):
-		if assembly[-i:] == seq[:i]:
-			addition = len(seq) - i
-			if next_move[1] > addition:
-				next_move = (seq, addition, "sx")			
-	# check if as prefix is shorter
-	half = len(seq)/2
-	for i in range(half):
-		if assembly[:i] == seq[-i:]:
-			addition = len(seq) - i
-			if next_move[1] > addition:
-				next_move = (seq, addition, "px")
-			
-if next_move[2] == "sx":
-	assembly = assembly + next_move[0][-next_move[1]:]
-elif next_move[2] == "px":
-	assembly = next_move[0][:next_move[1]] + assembly
-seqs.remove(next_move[0])
-
-print(seqs)
-print(assembly)
-
-next_move = ("",len(assembly),"XX")
-for seq in seqs:
-	# check if as sufix is shorter
-	half = len(seq)/2
-	for i in range(half,len(seq)):
-		if assembly[-i:] == seq[:i]:
-			addition = len(seq) - i
-			if next_move[1] > addition:
-				next_move = (seq, addition, "sx")			
-	# check if as prefix is shorter
-	half = len(seq)/2
-	for i in range(half):
-		if assembly[:i] == seq[-i:]:
-			addition = len(seq) - i
-			if next_move[1] > addition:
-				next_move = (seq, addition, "px")
-			
-if next_move[2] == "sx":
-	assembly = assembly + next_move[0][-next_move[1]:]
-elif next_move[2] == "px":
-	assembly = next_move[0][:next_move[1]] + assembly
-seqs.remove(next_move[0])
-
-print(seqs)
 print(assembly)
